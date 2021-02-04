e761b0fce81e8136fc102fccacb3d51953c1b4f2
lubis <lubis.l.aa@m.titech.ac.jp>
2021-02-05 04:25:42 +0900

    Constraint generation for array

--- testfiles/java-src/mid/empty/ver1.Empty.java
+++
@@ -1,8 +0,0 @@
-package mid.empty.ver1;
-
-/**
- *  There is no need for version inference in the upstream package
- */
-public class Empty {
-  public Empty() {}
-}
--- testfiles/java-src/mid/empty/ver2.Empty.java
+++
@@ -1,8 +0,0 @@
-package mid.empty.ver2;
-
-/**
- *  There is no need for version inference in the upstream package
- */
-public class Empty {
-  public Empty() { }
-}
